3d916e6c5268644d4b9736f5d2a5990ef1d5ea59
Huy <97140326+cut113@users.noreply.github.com>
2022-10-03 12:31:32 +0700

    Lab 4

--- js/scripts.js
+++ js/scripts.js
@@ -39,3 +39,10 @@ $(document).ready(function () {
     }
     
     });
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('toggle');
+    });
+    
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle');
+    });
